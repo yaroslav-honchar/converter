7c109484144a714d59130b93edc33ccbe0a3d655
Yaroslav Honchar <yaroslav.honchar.1@gmail.com>
2024-05-22 23:07:07 +0300

    fix: api route clean up

--- app/api/convert-image/route.ts
+++ app/api/convert-image/route.ts
@@ -2,7 +2,7 @@ import { NextRequest } from "next/server"
 // import { promises as fs } from "fs"
 // import path from "path"
 // import { fileURLToPath } from "url"
-//
+
 // const __filename = fileURLToPath(import.meta.url)
 // const __dirname = path.dirname(__filename)
 
@@ -15,7 +15,7 @@ export async function POST(req: NextRequest) {
     }
 
     // const buffer = await file.arrayBuffer()
-    //
+
     // await fs.writeFile(path.join(__dirname, "uploads", file.name), Buffer.from(buffer))
 
     return Response.json({ message: "File uploaded successfully" })
@@ -23,23 +23,3 @@ export async function POST(req: NextRequest) {
     return Response.json({ error: "Error handling file upload" })
   }
 }
-
-// export async function POST(req: NextRequest) {
-//   try {
-//     const data = await req.formData()
-//     const file = data.get("file") as File
-//     if (!file) {
-//       return Response.json({ error: "Error handling file upload" })
-//     }
-//
-//     const buffer = await file.arrayBuffer()
-//
-//     await fs.writeFile(path.join(__dirname, "uploads", file.name), Buffer.from(buffer))
-//
-//     return Response.json({ message: "File uploaded successfully" })
-//   } catch (error) {
-//     console.error("Error handling file upload:", error)
-//
-//     return Response.json({ error: "Error handling file upload" })
-//   }
-// }
