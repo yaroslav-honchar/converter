7dc2754ee4cb99a194cb6599f251255475b41a7c
Yaroslav Honchar <yaroslav.honchar.1@gmail.com>
2024-06-07 14:13:05 +0300

    feat: styled lang switcher

--- src/widget/layout/ui/layout.widget.tsx
+++ src/widget/layout/ui/layout.widget.tsx
@@ -6,7 +6,6 @@ import { ClientProvider, ServerProvider } from "@/_app/providers"
 import { BackgroundVideo } from "@/entities/background-video"
 import cn from "classnames"
 import { interFont, urbanistFont } from "@/_app/fonts"
-import { Button } from "primereact/button"
 
 export function LayoutRoot({ children, params: { locale } }: ILayoutRootProps) {
   return (
@@ -19,34 +18,7 @@ export function LayoutRoot({ children, params: { locale } }: ILayoutRootProps) {
             <div className={"flex flex-col min-h-screen relative"}>
               <BackgroundVideo />
               <Header />
-              <main className={"flex-grow flex flex-col"}>
-                <Button label="Primary" />
-                <Button
-                  label="Secondary"
-                  severity="secondary"
-                />
-                <Button
-                  label="Success"
-                  severity="success"
-                />
-                <Button
-                  label="Info"
-                  severity="info"
-                />
-                <Button
-                  label="Warning"
-                  severity="warning"
-                />
-                <Button
-                  label="Help"
-                  severity="help"
-                />
-                <Button
-                  label="Danger"
-                  severity="danger"
-                />
-                {children}
-              </main>
+              <main className={"flex-grow flex flex-col"}>{children}</main>
               <Footer />
             </div>
           </ClientProvider>
